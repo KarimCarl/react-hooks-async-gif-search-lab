35aef789016f3763eb0c9a223db4bb4cdaf5704a
KarimCarl <cndungu109@gmail.com>
2023-01-22 22:18:10 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,20 @@
 import React from "react";
+import GifSearch from "./GifSearch";
 
 function NavBar({ color, title }) {
   const colors = {
     black: "navbar-inverse",
     white: "navbar-default",
   };
-
   return (
     <nav className={`navbar ${colors[color]}`}>
       <div className="container-fluid">
         <div className="navbar-header">
           <span className="navbar-brand">{title}</span>
         </div>
+        <GifSearch />
       </div>
     </nav>
   );
 }
-
 export default NavBar;
